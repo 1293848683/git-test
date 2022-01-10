cf36400d148f60c83df7ae00407a1f14da8f8d28
liuhong <liuhong@atguigu.com>
2022-01-10 09:48:33 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Git.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author LiuHong
+ * @create 2022-01-10 9:38
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
