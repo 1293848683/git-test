cabeab209689832673d54a4da40395406d967a21
liuhong <liuhong@atguigu.com>
2022-01-10 10:56:16 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -11,5 +11,7 @@ public class Git {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("hello git5!");
+        System.out.println("hello git5!");
+        System.out.println("hello git5!");
     }
 }
